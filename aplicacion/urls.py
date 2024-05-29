36a941c324ab36eb1e9d34c7f125e5b163fdeac2
xJonathanMx <jon.molina@duocuc.cl>
2024-05-29 01:38:35 -0400

    Actualizacion de server

--- aplicacion/urls.py
+++ aplicacion/urls.py
@@ -1,8 +1,16 @@
 
 
 from django.urls import path,include
-from .views import index
+from .views import index,carta,pedidos,pagar,carrito,tabla_pedidos,registro_usuario,login,admin
 
 urlpatterns = [
-    path('',index,name='index')
+    path('',index,name='index'),
+    path('carta/',carta,name='carta'),
+    path('pedidos/',pedidos,name='pedidos'),
+    path('pagar/',pagar,name='pagar'),
+    path('carrito/',carrito,name='carrito'),
+    path('tabla pedidos/',tabla_pedidos,name='tabla_pedidos'),
+    path('registro/',registro_usuario,name='registro_usuario'),
+    path('login/',login,name='login'),
+    path('admin/',admin,name='admin')
 ]
--- aplicacion/views.py
+++ aplicacion/views.py
@@ -3,3 +3,25 @@ from django.shortcuts import render
 # Create your views here.
 def index(request):
     return render(request,'aplicacion/index.html')
+
+def carta(request):
+    return render(request,'aplicacion/carta.html')
+
+def pedidos(request):
+    return render(request,'aplicacion/pedidos.html')
+
+def pagar(request):
+    return render(request,'aplicacion/pagar.html')
+
+def registro_usuario(request):
+    return render(request,'aplicacion/registro-usuario.html')
+def login(request):
+    return render (request,'aplicacion/login.html')
+
+def tabla_pedidos(request):
+    return render(request,'aplicacion/tabla_pedidos.html')
+
+def carrito(request):
+    return render(request,'aplicacion/carrito.html')
+def admin(request):
+    return render(request,'aplicacion/admin.html')
