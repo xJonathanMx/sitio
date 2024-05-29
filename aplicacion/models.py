8869f16932c646c37d8ecfd082f1133a6250b608
xJonathanMx <jon.molina@duocuc.cl>
2024-05-29 18:16:25 -0400

    comentarios model

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -3,25 +3,31 @@ from django.db import models
 # Create your models here.
 
 
-class Persona(models.Model):
-    rut=models.CharField(max_length=10, primary_key=True)
-    nombre=models.CharField( max_length=50,null=False)
-    apellido=models.CharField(max_length=50,null=False)
-    f_nacto=models.DateField(max_length=50,)
+
+
+
+
+
+
+#class Persona(models.Model):
+    #rut=models.CharField(max_length=10, primary_key=True)
+    #nombre=models.CharField( max_length=50,null=False)
+    #apellido=models.CharField(max_length=50,null=False)
+    #f_nacto=models.DateField(max_length=50,)
     
     
-    def __str__(self):
-        return f"{self.rut}-{self.nombre}-{self.apellido}-{self.f_nacto}"
+    #def __str__(self):
+        #return f"{self.rut}-{self.nombre}-{self.apellido}-{self.f_nacto}"
     
-class Mascota(models.Model):
-        nombre=models.CharField( max_length=50,null=False)
-        edad=models.IntegerField()
-        tipo=models.CharField(max_length=20) 
-        #Dueño de la mascota
-        propietario=models.ForeignKey(Persona, on_delete=models.PROTECT)
+# #class Mascota(models.Model):
+#         #nombre=models.CharField( max_length=50,null=False)
+#         edad=models.IntegerField()
+#         tipo=models.CharField(max_length=20) 
+#         #Dueño de la mascota
+#         propietario=models.ForeignKey(Persona, on_delete=models.PROTECT)
         
-        def __str__(self):
-            return self.nombre +"-"+self.tipo
+#         def __str__(self):
+#             return self.nombre +"-"+self.tipo
         
     
 
