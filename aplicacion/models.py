d2055fad1a452c50a1036c1d34b7d718650005dd
xJonathanMx <jon.molina@duocuc.cl>
2024-06-07 17:13:45 -0400

    bla bla bla

--- aplicacion/models.py
+++ aplicacion/models.py
@@ -25,6 +25,8 @@ class Producto(models.Model):
     nom_producto = models.CharField(max_length=100)
     descripcion = models.TextField()
     valor = models.DecimalField(max_digits=10, decimal_places=2)
+    imagen=models.ImageField(upload_to='Producto',null=True)
+
 
     def __str__(self):
         return self.nom_producto
